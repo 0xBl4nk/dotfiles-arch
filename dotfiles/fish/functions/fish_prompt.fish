0812b2b1aaaf26a818951b2c97d1488ba53a481f
0xBl4nk <61481946+0xBl4nk@users.noreply.github.com>
2025-04-07 10:10:41 -0300

    add

---
+++ dotfiles/fish/functions/fish_prompt.fish
@@ -0,0 +1,19 @@
+function fish_prompt
+    # Set the color and print the current working directory
+    set_color cyan
+    printf '🪷 %s' (prompt_pwd)
+
+    # Check for Git directory and show Git branch
+    if type -q git
+        set -l git_branch (git branch --show-current 2>/dev/null)
+        if test -n "$git_branch"
+            set_color purple
+            printf " [%s]" $git_branch
+            set_color normal
+        end
+    end
+    
+    # Final arrow
+    echo -n " ➤ "
+    echo ''
+end
