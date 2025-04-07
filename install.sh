f71c751fe13e6c526ee64e8f02033044df5ee57a
0xBl4nk <61481946+0xBl4nk@users.noreply.github.com>
2025-04-07 10:13:01 -0300

    script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+pacman -S hyprpaper
+pacman -S waybar
+pacman -S ttf-nerd-fonts-symbols
+pacman -S noto-fonts-emoji
+pacman -S noto-fonts
+pacman -S nodejs
+pacman -S npm
+pacman -S nvim
+pacman -R dolphin
+pacman -S nautilus
+pacman -S btop
+pacman -S pavucontrol
